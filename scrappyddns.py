be9aa6367d46723e4c2a5eb90d82a288f2e0e666
Rob Hasselbaum <rob@hasselbaum.net>
2015-03-28 09:36:38 -0400

    Update docstring.

--- scrappyddns.py
+++ scrappyddns.py
@@ -1,3 +1,7 @@
+"""A Flask web service that listens for requests from authorized DDNS clients
+and sends mobile push notifications on IP address changes.
+"""
+
 import logging.handlers
 import os
 from flask import Flask, abort, request
